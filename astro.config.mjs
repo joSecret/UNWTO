e2326c5b7797943521b498b7abd4069e9956162f
Ion Obreja <jo@da.md>
2024-04-14 20:26:24 +0300

    page git config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://joSecret.github.io',
-  base: '/jo-base-theme',
+  base: '/unwto',
 	integrations: [mdx(), sitemap()],
 });
